339c4449565080b323cecee3f5e4fd25c1bdf9f2
davronmamaraimov1992@gmail.com <davronmamaraimov1996@gmail.com>
2023-08-04 10:51:47 +0500

    Header is done

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,10 @@
 import React, {Component} from "react";
+import "bootstrap-icons/font/bootstrap-icons.css";
+import { MenuUp } from 'react-bootstrap-icons';
+import { Search } from 'react-bootstrap-icons';
+import { Person } from 'react-bootstrap-icons';
+import { Heart } from 'react-bootstrap-icons';
+import { Cart } from 'react-bootstrap-icons';
 
 class Header extends Component{
     render(){
@@ -6,18 +12,16 @@ class Header extends Component{
             <div className="header">
                 <nav>
                     <div className="left-box">
-                        <a href="#!">icon</a>
+                        <a href="#!"><MenuUp className="menu-icon"/></a>
                         <div className="input-box">
-                            <input type="text" />
+                            <Search className="search"/>
+                            <input type="text" placeholder="What are you looking for ?"/>
                         </div>
                     </div>
                     <div className="right-box">
-                        <a href="#!">P</a>
-                        <a href="#!">H</a>
-                        <a href="#!">
-                            <i></i>
-                            
-                        </a>
+                        <a href="#!" className="person-icon"><Person /></a>
+                        <a href="#!" className="heart-icon"><Heart /></a>
+                        <a href="#!" className="basket"><Cart className="cart"/> <h4>Basket</h4></a>
                     </div>
                 </nav>
             </div>
